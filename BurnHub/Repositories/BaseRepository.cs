3c7a2e25adb2378cdad6481c84e508afd4b7fc3b
Danny Herrmann <dannyherrmann91@gmail.com>
2023-04-18 19:36:31 -0500

    Base Repo and DBUTILS

---
+++ BurnHub/Repositories/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace BurnHub.Repositories;
+
+public abstract class BaseRepository
+{
+    private readonly string _connectionString;
+
+    public BaseRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    protected SqlConnection Connection
+    {
+        get
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
