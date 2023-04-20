1edb79a65622188bbca192f9fe5194033ba7be9b
Mason Wheaton <mlwheaton06@gmail.com>
2023-04-19 21:17:14 -0500

    Made and tested Model, Repo, and Controller for User with basic CRUD

---
+++ BurnHub/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using BurnHub.Models;
+using BurnHub.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurnHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UserController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userRepo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _userRepo.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            _userRepo.Add(user);
+            return CreatedAtAction("Get", new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _userRepo.Update(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepo.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ BurnHub/Repositories/UserRepository.cs
@@ -0,0 +1,171 @@
+﻿using BurnHub.Models;
+using BurnHub.Utils;
+
+namespace BurnHub.Repositories;
+
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public UserRepository(IConfiguration configuration) : base(configuration) { }
+
+    public List<User> GetAll()
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"SELECT
+                                        id,
+	                                    name,
+	                                    isSeller,
+	                                    dateCreated,
+	                                    email,
+	                                    firebaseId,
+	                                    image
+                                    FROM [User]";
+
+                var reader = cmd.ExecuteReader();
+                var users = new List<User>();
+
+                while (reader.Read())
+                {
+                    var user = new User()
+                    {
+                        Id = DbUtils.GetInt(reader, "id"),
+                        Name = DbUtils.GetString(reader, "name"),
+                        IsSeller = DbUtils.GetBoolean(reader, "isSeller"),
+                        DateCreated = DbUtils.GetDateTime(reader, "dateCreated"),
+                        Email = DbUtils.GetString(reader, "email"),
+                        FirebaseId = DbUtils.GetString(reader, "firebaseId"),
+                        Image = DbUtils.GetString(reader, "image")
+                    };
+
+                    users.Add(user);
+                }
+
+                reader.Close();
+                return users;
+            }
+        }
+    }
+
+    public User GetById(int id)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"SELECT
+                                        id,
+	                                    name,
+	                                    isSeller,
+	                                    dateCreated,
+	                                    email,
+	                                    firebaseId,
+	                                    image
+                                    FROM [User]
+                                    WHERE id = @id";
+                DbUtils.AddParameter(cmd, "@id", id);
+
+                var reader = cmd.ExecuteReader();
+
+                User user = null;
+                if (reader.Read())
+                {
+                    user = new User()
+                    {
+                        Id = DbUtils.GetInt(reader, "id"),
+                        Name = DbUtils.GetString(reader, "name"),
+                        IsSeller = DbUtils.GetBoolean(reader, "isSeller"),
+                        DateCreated = DbUtils.GetDateTime(reader, "dateCreated"),
+                        Email = DbUtils.GetString(reader, "email"),
+                        FirebaseId = DbUtils.GetString(reader, "firebaseId"),
+                        Image = DbUtils.GetString(reader, "image")
+                    };
+                }
+
+                reader.Close();
+                return user;
+            }
+        }
+    }
+
+    public void Add(User user)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"INSERT INTO [User]
+	                                    (name,
+	                                    isSeller,
+	                                    dateCreated,
+	                                    email,
+	                                    firebaseId,
+	                                    image)
+                                    OUTPUT INSERTED.ID
+                                    VALUES
+	                                    (@name,
+                                        @isSeller,
+                                        @dateCreated,
+                                        @email,
+                                        @firebaseId,
+                                        @image)";
+
+                DbUtils.AddParameter(cmd, "@name", user.Name);
+                DbUtils.AddParameter(cmd, "@isSeller", user.IsSeller);
+                DbUtils.AddParameter(cmd, "@dateCreated", user.DateCreated);
+                DbUtils.AddParameter(cmd, "@email", user.Email);
+                DbUtils.AddParameter(cmd, "@firebaseId", user.FirebaseId);
+                DbUtils.AddParameter(cmd, "@image", user.Image);
+
+                user.Id = (int)cmd.ExecuteScalar();
+            }
+        }
+    }
+
+    public void Update(User user)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = @"UPDATE [User]
+	                                    SET name = @name,
+	                                        isSeller = @isSeller,
+	                                        dateCreated = @dateCreated,
+	                                        email = @email,
+	                                        firebaseId = @firebaseId,
+	                                        image = @image
+                                    WHERE id = @id";
+
+                DbUtils.AddParameter(cmd, "@id", user.Id);
+                DbUtils.AddParameter(cmd, "@name", user.Name);
+                DbUtils.AddParameter(cmd, "@isSeller", user.IsSeller);
+                DbUtils.AddParameter(cmd, "@dateCreated", user.DateCreated);
+                DbUtils.AddParameter(cmd, "@email", user.Email);
+                DbUtils.AddParameter(cmd, "@firebaseId", user.FirebaseId);
+                DbUtils.AddParameter(cmd, "@image", user.Image);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void Delete(int id)
+    {
+        using (var conn = Connection)
+        {
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = "DELETE FROM [User] WHERE id = @id";
+                DbUtils.AddParameter(cmd, "@id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
