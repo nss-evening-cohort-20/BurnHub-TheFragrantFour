9193d1a29b72b5a60fbc93c3f194dc4735a66061
Danny <dannyherrmann91@gmail.com>
2023-04-23 23:31:23 -0500

    fixing option controller and image on item model

--- BurnHub/Models/Item.cs
+++ BurnHub/Models/Item.cs
@@ -9,6 +9,7 @@ public class Item
     public string? Description { get; set; }
     public int Price{ get; set; }
     public int Quantity { get; set; }
+    public string Image { get; set; }
     //public Category? Category { get; set; }
     //public Store? Store { get; set; }
 }
