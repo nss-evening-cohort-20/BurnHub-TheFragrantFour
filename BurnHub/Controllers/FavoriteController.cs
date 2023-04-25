6f0d08d738af267424ba3e8171625b831839b01e
Kyle <kylebennett9514@gmail.com>
2023-04-24 21:35:14 -0500

    Created Favorite model, repo and controller

---
+++ BurnHub/Controllers/FavoriteController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurnHub.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        // GET: FavoriteController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FavoriteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FavoriteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FavoriteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FavoriteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FavoriteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FavoriteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FavoriteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BurnHub/Models/Favorite.cs
@@ -0,0 +1,9 @@
+﻿namespace BurnHub.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int UserId { get; set; }
+    }
+}
