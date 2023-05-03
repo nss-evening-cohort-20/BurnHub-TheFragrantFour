226900abc081dda5b5ed3cff5d0f3a3b12681db8
Danny Herrmann <dannyherrmann91@gmail.com>
2023-05-02 21:07:41 -0500

    items work cors fix

--- burnhub-client/src/components/APIManager.js
+++ burnhub-client/src/components/APIManager.js
@@ -1,3 +1,5 @@
-export const APIManager = () => {
-
+export const FetchItems = async () => {
+    const response = await fetch(`https://localhost:7069/Items`);
+    const itemsArray = await response.json();
+    return itemsArray
 }
