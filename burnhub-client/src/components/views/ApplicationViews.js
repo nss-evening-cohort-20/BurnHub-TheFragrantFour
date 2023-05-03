d70dadaff1105eacfa2971e84d67badf36453fe9
Kyle <kylebennett9514@gmail.com>
2023-05-02 21:29:49 -0500

    Fixed issue in Item getById SQL

--- burnhub-client/src/components/views/ApplicationViews.js
+++ burnhub-client/src/components/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from "react-router-dom";
 import { Home } from "../home/Home";
 import { NavBar } from "../nav/NavBar";
 import { ProductForm } from "../productForm/ProductForm";
+import { Profile } from "../profile/Profile";
 
 export const ApplicationViews = () => {
 
@@ -15,6 +16,7 @@ export const ApplicationViews = () => {
             }>
             <Route path="/" element={ <Home /> } />
             <Route path="ProductForm" element={<ProductForm />} />
+            <Route path="/:userId" element={<Profile />} />
             {/* <Route path="/store" element={ <Store /> } />
             <Route path="/" element={ <ItemPage /> } />
             <Route path="/" element={ <ViewStores /> } /> */}
