619fed4441510438a2c213f5aeb1c263fc228fad
erica-clayton <erica.baertsch@gmail.com>
2023-05-02 21:02:09 -0500

    working on store component

--- burnhub-client/src/components/myStore/Store.js
+++ burnhub-client/src/components/myStore/Store.js
@@ -1,18 +1,24 @@
 import { useState } from "react"
 
 export const Store = () => {
+    const [stores, setStores] = useState([])
+
+    useEffect(() => {
+        const storesFetch = async () => {
+          const response = await fetch(`https://localhost:7069/Stores`);
+          const storesData = await response.json();
+          setStores(storesData);
+        };
+        storesFetch();
+      }, []);
 
     return <main>
-        <div>Store</div>
-        <div>Store</div>
-        <div>Store</div>
-        <div>Store</div>
-        <div>Store</div>
-        <div>Store</div>
-        <div>Store</div>
-        <div>Store</div>
-
-
+        <div>
+            {stores.map((store) => (
+                
+             ))}
+        </div>
+        
 
     </main>
 }
